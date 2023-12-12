e000ab39a375886b8504481498baa2a55a48eec4
Vipul-Kamble2812 <vipulkamble2812@gmail.com>
2023-12-12 23:45:14 +0530

    third-commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-dark text-light fs-3 d-flex justify-content-center position-absolute bottom-0 start-0 w-100'>
+    <footer className='bg-dark text-light fs-3 d-flex justify-content-center position-absolute bottom-0 start-0 w-100'>
+      <div>
       &copy; All rights are reserved | mycalculator.com
     </div>
+    </footer>
   )
 }
 
